0940ca70bc8bea6aad183cbbde878c3f5362a4c5
jordillps <jordillps@gmail.com>
2019-09-05 12:48:38 +0200

    Fifth Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 Route::get('/', function () { return redirect('/admin/home'); });
 
+
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('auth.login');
 $this->post('login', 'Auth\LoginController@login')->name('auth.login');
